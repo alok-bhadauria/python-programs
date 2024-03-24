41658d24613e6aaa09430c0caf60b820d67efe9c
Alok Bhadauria <142592948+alok-bhadauria@users.noreply.github.com>
2024-03-24 20:35:25 +0530

    Add files via upload

---
+++ Series questions/fibonacci series.py
@@ -0,0 +1,11 @@
+# n = int(input())
+
+# ls = [0,1]
+
+# for i in range(2, n+2):
+#     a = ls[-2]
+#     b = ls[-1]
+#     x = a + b
+#     ls.append(x)
+    
+# print(ls)
---
+++ Series questions/tribonacci series.py
@@ -0,0 +1,21 @@
+# n = int(input())
+
+# ls = [0, 0, 1]
+
+# a = ls[0]
+# b = ls[1]
+# c = ls[2]
+
+# for i in range(1,n+1):
+#     x = a + b + c
+#     ls.append(x)
+
+#     a = ls[-3]
+#     b = ls[-2]
+#     c = ls[-1]
+
+# print(ls)
+
+a = eval(input())
+
+print(a)
